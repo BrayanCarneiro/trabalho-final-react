2eb043a5158e09d6d1dfd35cff6fb2311d2c2b34
Mariana Hiath <maarianahiath@gmail.com>
2021-11-24 23:43:31 -0300

    teste numero um milhão

--- trabalhofinal/src/App.jsx
+++ trabalhofinal/src/App.jsx
@@ -9,37 +9,6 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>será que vai</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            clique aqui: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
     </div>
   )
 }
