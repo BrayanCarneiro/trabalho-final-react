c5d26ae523f085ce8d6f0256000943da80d33790
Mariana Hiath <maarianahiath@gmail.com>
2021-11-24 23:31:25 -0300

    teste

--- trabalhofinal/src/App.jsx
+++ trabalhofinal/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import NavBar from './NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>será que vai</p>
--- trabalhofinal/src/NavBar.jsx
+++ trabalhofinal/src/NavBar.jsx
@@ -1,4 +1,4 @@
-function NavBar() {
+export default function NavBar() {
 
     return (
         <>
