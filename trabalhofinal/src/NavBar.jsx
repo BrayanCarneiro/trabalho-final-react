1589ab4d232cf96d570bdfa8b457415929908d82
Mariana Hiath <maarianahiath@gmail.com>
2021-11-24 23:29:58 -0300

    teste

---
+++ trabalhofinal/src/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <h1>mariana</h1>
+                </nav>
+            </header>
+        </>
+    )
+}
